cab4958ef3e63ad8d1b21fce9d8521495a237d98
mertakanay <mert.akanay@live.com>
2015-07-11 12:39:42 -0700

    Initial Commit

---
+++ Clairify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Clairify
+//
+//  Created by Mert Akanay on 7/11/15.
+//  Copyright (c) 2015 mertakanay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
